438718589bdea09cb6b27f338f62c95492048bed
ryohey <info@codingcafe.jp>
2023-08-03 12:30:52 +0900

    Add GraphQL Codegen

---
+++ codegen.ts
@@ -0,0 +1,25 @@
+import { CodegenConfig } from "@graphql-codegen/cli"
+
+const config: CodegenConfig = {
+  schema: {
+    "https://api.github.com/graphql": {
+      headers: {
+        Authorization: `Bearer ${process.env.VITE_GITHUB_TOKEN}`,
+        "User-Agent": "graphql-codegen",
+      },
+    },
+  },
+  documents: ["src/**/*.tsx"],
+  generates: {
+    "./src/__generated__/": {
+      preset: "client",
+      plugins: [],
+      presetConfig: {
+        gqlTagName: "gql",
+      },
+    },
+  },
+  ignoreNoDocuments: true,
+}
+
+export default config
--- src/RepositoryList.tsx
+++ src/RepositoryList.tsx
@@ -1,23 +1,26 @@
-import { gql, useQuery } from "@apollo/client"
+import { useQuery } from "@apollo/client"
+import { gql } from "./__generated__"
 
-export function RepositoryList() {
-  const { loading, error, data } = useQuery(gql`
-    query {
-      search(query: "swift", type: REPOSITORY, first: 10) {
-        nodes {
-          ... on Repository {
-            id
-            name
-          }
+const GET_REPOSITORIES = gql(/* GraphQL */ `
+  query GetRepositories {
+    search(query: "swift", type: REPOSITORY, first: 10) {
+      nodes {
+        ... on Repository {
+          id
+          name
         }
       }
     }
-  `)
+  }
+`)
+
+export function RepositoryList() {
+  const { loading, error, data } = useQuery(GET_REPOSITORIES)
 
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error :(</p>
 
-  return data.search.nodes.map(({ name, id }: any) => (
+  return data?.search?.nodes?.map(({ name, id }: any) => (
     <div key={id}>
       <p>{name}</p>
     </div>
